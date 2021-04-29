395908443924bcf3419c90ed173339e3278b6fac
Khaled Mohammad <itskhaledmohammad@gmail.com>
2021-04-29 16:35:03 +0600

    :sparkles: Completed the initialization of the app.

---
+++ src/app.ts
@@ -0,0 +1,24 @@
+require('module-alias/register')
+import express from "express";
+import morgan from "morgan";
+import helmet from "helmet";
+import cors from "cors";
+
+// Initializing our express app 🎉
+const app: express.Application = express();
+
+// Some Security Headers. 👀
+app.use(helmet());
+
+// Enabling CORS for all routes. 🛩
+app.use(cors());
+
+// Logging 🖊️
+app.use(morgan(':remote-addr - :remote-user [:date[clf]] :method :url HTTP/:http-version :status [content-length: :res[content-length]] - [referrer: :referrer] - [user-agent: :user-agent] - [response-time: :response-time ms] - [total-time: :total-time ms]'));
+
+//  Parsing the body. 💪
+app.use(express.json())
+app.use(express.urlencoded());
+
+// Exporting our app 💯 
+module.exports = app;
