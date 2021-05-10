30b5c4cbacc4fd9285731eb7cc6307c226b54cf6
Khaled Mohammad <itskhaledmohammad@gmail.com>
2021-05-11 04:50:29 +0600

    Created Axios singleton to be used all over the application.

---
+++ dist/api/v1/utils/axios.js
@@ -0,0 +1,11 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const axios_1 = __importDefault(require("axios"));
+const axiosInstance = axios_1.default.create({
+    baseURL: `${process.env.API_BASE_URL}`,
+});
+exports.default = axiosInstance;
+//# sourceMappingURL=axios.js.map
---
+++ src/api/v1/utils/axios.ts
@@ -0,0 +1,9 @@
+
+import axios, { AxiosInstance } from 'axios';
+
+const axiosInstance:AxiosInstance = axios.create({
+    baseURL: `${process.env.API_BASE_URL}`,
+ 
+});
+
+export default axiosInstance;
