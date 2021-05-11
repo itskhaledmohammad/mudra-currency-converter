1e265848d3c9a7492ea092a73233c23ebe9f3eb5
Khaled Mohammad <itskhaledmohammad@gmail.com>
2021-05-11 22:50:57 +0600

    Added luxon and types for typescript support.

--- src/api/v1/symbols/symbols.controller.ts
+++ src/api/v1/symbols/symbols.controller.ts
@@ -1,6 +1,8 @@
 import express from "express"
 import axiosInstance from "@api/v1/utils/axios";
 import {IFixerAPIData, IAPIResponse} from "./symbols.interface"
+import {DateTime} from "luxon"
+
 async function listSymbols(req: express.Request, res: express.Response) { 
     const symbols = await axiosInstance.get(`/symbols?access_key=${process.env.FIXER_API_KEY}`);
     return res.status(200).json(symbols)
@@ -15,5 +17,13 @@ async function convertCurrency(req: express.Request, res: express.Response) {
     const {base, target} = req.params;
     const allCurrencies:IFixerAPIData = await axiosInstance.get(`/latest?access_key=${process.env.FIXER_API_KEY}`);
     const rate = allCurrencies.rates[target] / allCurrencies.rates[base]
-    return res.status(200).json(allCurrencies)
+    const response: IAPIResponse = {
+        success: true,
+        timestamp: DateTime.now().toMillis(),
+        date: DateTime.now().toISODate(),
+        base,
+        target,
+        rate
+    }
+    return res.status(200).json(response)
 }
