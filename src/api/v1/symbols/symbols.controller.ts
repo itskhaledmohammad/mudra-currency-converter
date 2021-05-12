6ca119115cd18521d71973bb177960fc166082ea
Khaled Mohammad <itskhaledmohammad@gmail.com>
2021-05-12 06:28:52 +0600

    Throw error if any of the currency is invalid.

--- src/api/v1/symbols/symbols.controller.ts
+++ src/api/v1/symbols/symbols.controller.ts
@@ -19,7 +19,9 @@ async function convertCurrency(req: express.Request, res: express.Response) {
     const {base = "USD", target} = JSON.parse(JSON.stringify(req.query));
     const {data}: {data: IFixerAPICurrencyResponse} = await axiosInstance.get(`/latest?access_key=${process.env.FIXER_API_KEY}`);
     const rate = data.rates[target] / data.rates[base]
-
+    if(!rate) {
+        return res.status(422).json({"error": "Invalid currency type."})
+    }
     const response: IAPICurrencyResponse = {
         success: true,
         timestamp: DateTime.now().toMillis(),
