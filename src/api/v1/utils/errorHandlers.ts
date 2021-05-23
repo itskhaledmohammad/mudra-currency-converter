55ef5a13f639d3dfd005c6b898d332ba9f696c1e
Khaled Mohammad <itskhaledmohammad@gmail.com>
2021-05-24 04:45:48 +0600

    Added Eslint.

---
+++ src/api/v1/utils/errorHandlers.ts
@@ -0,0 +1,16 @@
+import express from "express"
+export const catchErrors = (fn: Function) => {
+    return (req: express.Request, res: express.Response, next: express.NextFunction) => {
+        return fn(req, res, next).catch(next)
+    }
+}
+
+export const developmentErrors = (err:Error, req:express.Request, res: express.Response,
+                           next: express.NextFunction) => {
+    err.stack = err.stack || '';
+    const errorDetails = {
+      error: err.message,
+      status_code: 500,
+    };
+    res.status(500).json(errorDetails);
+};
